1509d46c56fda43146ce49788505e4485ccc6bd5
FloncDev <37045958+FloncDev@users.noreply.github.com>
2025-07-23 02:16:20 +0100

    chore: axum boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    tracing::info!("Starting server...");
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .expect("Failed to bind to :3000");
+
+    tracing::info!("Server running at http://127.0.0.1:3000");
+
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to start server");
 }
